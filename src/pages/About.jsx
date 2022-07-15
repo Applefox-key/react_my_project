454790a77366774fb81dacec8e46407322b2d53a
Applefox-key <applefox.key@gmail.com>
2022-07-15 17:34:40 +0200

    img resizing3

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,12 @@ import pct from "../img/pngkey.png";
 const About = () => {
   return (
     // <div className="d-flex mx-3">
-    <div>
+    <div className="mx-4">
       <img src={pct} className="aboutImg" />
-      <h1 className="display-5 text-center">
+      <h1 className="display-5 text-center aboutH">
         The simple and effective strategy of "the 90 second method"
       </h1>
-      <p>
+      <p className="aboutP">
         When you see a new word, you should write it down in your notes. But not
         just the word by itself. Give it some context. For example, don't just
         write the translation of depend. It's better to write down the phrase
@@ -18,20 +18,22 @@ const About = () => {
         the phrase when you need it. The word itself should be highlighted or
         underlined.
       </p>
-      <p>
+      <p className="aboutP">
         For 7 days, you should read this expression out loud 1-2 times. Don't
         make yourself necessarily memorize it, just concentrate on it and try to
         understand what you are really saying. It takes 10 seconds to read a
         phrase twice.
       </p>
-      <p>
+      <p className="aboutP">
         After 7 days of everyday repetition, you should take a one-week break.
         After the break, read the phrase 3 times in 10 seconds. You should do it
         out loud, emotionally and clearly, with full concentration.
       </p>
-      <p>2 weeks later, read it out loud again (3 times in 10 seconds).</p>
+      <p className="aboutP">
+        2 weeks later, read it out loud again (3 times in 10 seconds).
+      </p>
       <h3>70 + 10 + 10 = 90 seconds.</h3>
-      <p>
+      <p className="aboutP">
         This strategy allows you to cement the expression in your memory. All of
         this has been checked by the author of this method.
       </p>
