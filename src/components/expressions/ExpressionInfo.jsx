985f88cbe5d25394574b643841c4fc7fd0c2fe9c
Applefox-key <applefox.key@gmail.com>
2022-12-25 21:12:51 +0100

    added detailed history

--- src/components/expressions/ExpressionInfo.jsx
+++ src/components/expressions/ExpressionInfo.jsx
@@ -6,18 +6,14 @@ import MyModal from "../UI/MyModal";
 const ExpressionInfo = ({ visible, setVisible, expression }) => {
   let studyPlan = expression.studyPlan;
   let history = expression.userHistory;
-  let dif = expression.diffInDays;
   return (
     <MyModal
       title={expression.expression}
       subtitle={expression.phrase}
       showmodal={visible}
       setshowmodal={setVisible}
-      // dialogClassName="modal-info"
       dialogClassName={"modal-max"}>
       <div className="d-flex ">
-        {/* <div> {expression.exceededSkipsDays}</div> */}
-        <div>dif{dif}</div>
         <CardForList header="Study plan" subtitle="" list={studyPlan} />
         <CardForHistoryList header="History" subtitle="" list={history} />
       </div>
