4760b5cdd70d7bf7922ad11bba56b73e164ae3c0
Applefox-key <applefox.key@gmail.com>
2023-05-16 23:52:35 +0200

    color-base style class has been added

--- src/components/expressions/Training/NoWork.jsx
+++ src/components/expressions/Training/NoWork.jsx
@@ -5,7 +5,9 @@ import pict from "../../../img/sitting_man.png";
 const NoWork = () => {
   return (
     <div>
-      <h1 className="display-1">there are no expressions for training!</h1>
+      <h1 className="display-1 color-base">
+        there are no expressions for training!
+      </h1>
       <div className="color_container"></div>
       <Image src={pict} className="pict" />
     </div>
